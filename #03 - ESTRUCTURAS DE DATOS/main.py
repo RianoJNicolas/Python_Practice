b518ba3c5ad8e59059a9112a99f61c0f1feb4e04
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-01-24 16:45:47 -0500

    Actualizando ejercicio #03, Agregando comentarios iniciales

---
+++ #03 - ESTRUCTURAS DE DATOS/main.py
@@ -0,0 +1,16 @@
+# 1. Ejemplos de estructuras de datos
+
+# 1.1 Listas
+
+# 1.2 Tuplas
+
+# 1.3 Diccionarios
+
+# 1.4 Conjuntos (Sets)
+
+# 1.5 Colecciones
+
+# 2. Operando con las estructuras
+
+
+# DIFICULTAD EXTRA
