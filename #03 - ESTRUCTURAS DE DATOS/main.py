f711b839ba50e1928b1d5be1d1fefb0912fba444
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-01-26 17:19:17 -0500

    Actualizando ejercicio #03, Agregando ejemplo lista como pila

--- #03 - ESTRUCTURAS DE DATOS/main.py
+++ #03 - ESTRUCTURAS DE DATOS/main.py
@@ -1,9 +1,24 @@
+from collections import OrderedDict, defaultdict, deque
+from array import array
 # 1. Ejemplos de estructuras de datos
 
 # 1.1 Listas
 myList = [1, 2, 3, 4, 5]
+print(f'Lista = {myList}\n')
 
 ## 1.1.1 Listas como Pilas
+"""
+Ultimo elemento que se añade, es el primer elemento que se retira
+"""
+myStack = myList
+print(f'Pila inicial = {myStack}')
+myStack.append(6)
+myStack.append(7)
+print(f'Pila agregando elementos = {myStack}')
+myStack.pop()
+print(f'Pila quitando elemento = {myStack}')
+myStack.pop()
+print(f'Pila quitando elemento = {myStack}\n')
 
 ## 1.1.2 Listas como Colas
 
@@ -17,7 +32,7 @@ myDict = {1: "hola", 2: "mundo", 3: ",", 4: "soy", 5: "rianojnicolas"}
 mySet = {1, 2, 3}
 
 # 1.5 Colecciones
-from collections import OrderedDict, defaultdict
+
 
 ## 1.5.1 Diccionarios Ordenados
 myOd = OrderedDict()
@@ -31,8 +46,11 @@ prices = defaultdict(int)
 prices['carrots']
 print(f'Diccionario Predeterminado = {prices}')
 
+## 1.5.3 Queue (Cola)
+mydeque = deque([1, 2, 3, 4])
+print(f'La cola (deque) = {mydeque}')
+
 # 1.6 Matrices
-from array import array
 myArray = array('f', [1.0, 1.5, 2.0, 2.5])
 print(f'Matriz = {myArray}')
 
