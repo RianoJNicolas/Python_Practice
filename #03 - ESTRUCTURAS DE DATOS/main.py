4c70ddea8038f7fe1e4a1de611c462bd6c9104b2
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-01-25 23:56:40 -0500

    Actualizando ejercicio #03, Agregando estructuras basicas

--- #03 - ESTRUCTURAS DE DATOS/main.py
+++ #03 - ESTRUCTURAS DE DATOS/main.py
@@ -1,14 +1,18 @@
 # 1. Ejemplos de estructuras de datos
 
 # 1.1 Listas
+myList = [1, 2, 3, 4, 5]
 
 # 1.2 Tuplas
+myTuple = (1, 2, 3, 4, 5)
 
 # 1.3 Diccionarios
+myDict = {1: "hola", 2: "mundo", 3: ",", 4: "soy", 5: "rianojnicolas"}
 
 # 1.4 Conjuntos (Sets)
-
+mySet = {1, 2, 3}
 # 1.5 Colecciones
+
 
 # 2. Operando con las estructuras
 
