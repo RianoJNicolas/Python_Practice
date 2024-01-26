1fcc462a4dbccb9eaf6627ee5aac6d3d39c65184
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-01-26 16:50:33 -0500

    Actualizando ejercicio #03, Agregando ejemplos de colecciones

--- #03 - ESTRUCTURAS DE DATOS/main.py
+++ #03 - ESTRUCTURAS DE DATOS/main.py
@@ -3,6 +3,10 @@
 # 1.1 Listas
 myList = [1, 2, 3, 4, 5]
 
+## 1.1.1 Listas como Pilas
+
+## 1.1.2 Listas como Colas
+
 # 1.2 Tuplas
 myTuple = (1, 2, 3, 4, 5)
 
@@ -11,8 +15,26 @@ myDict = {1: "hola", 2: "mundo", 3: ",", 4: "soy", 5: "rianojnicolas"}
 
 # 1.4 Conjuntos (Sets)
 mySet = {1, 2, 3}
-# 1.5 Colecciones
 
+# 1.5 Colecciones
+from collections import OrderedDict, defaultdict
+
+## 1.5.1 Diccionarios Ordenados
+myOd = OrderedDict()
+myOd['first']  = 'one'
+myOd['second'] = 'two'
+myOd['third']  = 'three'
+print(f'Diccionario Ordenado {myOd}')
+
+## 1.5.2 Diccionario Predeterminado
+prices = defaultdict(int)
+prices['carrots']
+print(f'Diccionario Predeterminado = {prices}')
+
+# 1.6 Matrices
+from array import array
+myArray = array('f', [1.0, 1.5, 2.0, 2.5])
+print(f'Matriz = {myArray}')
 
 # 2. Operando con las estructuras
 
