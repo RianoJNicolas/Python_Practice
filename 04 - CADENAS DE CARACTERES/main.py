28d1bd24be7bf289a60da12b085fb40c0c767ddf
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-02-13 22:24:10 -0500

    Actualizando Reto #04, agregando primeras operaciones de strings

---
+++ 04 - CADENAS DE CARACTERES/main.py
@@ -0,0 +1,15 @@
+# OPERACIONES CON CADENAS DE CARACTERES
+
+# 1. Asignacion
+saludo = "Hola, "
+nombre = "Nico"
+
+# 2. Concatenacion
+sal = saludo + nombre
+print(sal)
+
+# 3. Operaciones "aritmeticas"
+saludoTotal = saludo*3
+print(saludoTotal)
+
+# 4. Operacion de partcion
