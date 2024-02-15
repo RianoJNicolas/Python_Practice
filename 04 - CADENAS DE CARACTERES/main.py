35a8e4c06c94a40c54e36ccc58efab72cec1154e
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-02-15 17:53:10 -0500

    Actualizando Reto #04, agregando subcadenas y acceso a caracteres

--- 04 - CADENAS DE CARACTERES/main.py
+++ 04 - CADENAS DE CARACTERES/main.py
@@ -12,6 +12,19 @@ print(sal)
 saludoTotal = saludo*3
 print(saludoTotal)
 
-# 4. Operacion de partcion
-primerCorte = saludo[0:2:1]
+# 4. Operaciones de partcion (Subcadena)
+secuencia = "123456789"
+primerCorte = secuencia[0:2:1] # Se toma -> 12
+segundoCorte = secuencia[::2] # Se toma -> 13579
+tercerCorte = secuencia[::3] # Se toma -> 147 
+cuartoCorte = secuencia[::] #Se toma toda la cadena
 print(primerCorte)
+print(segundoCorte)
+print(tercerCorte)
+print(cuartoCorte)
+
+# 5. Acceso a caracteres
+caracter1 = saludo[0]
+caracter2 = saludo[4]
+print(caracter1)
+print(caracter2)
