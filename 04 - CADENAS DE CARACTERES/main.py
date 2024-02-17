0ef1be510eecb4bdedd48b02cbd5a94f5b1bf8b1
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-02-16 21:11:55 -0500

    Actualizando Reto #04, agregando y aclarando el proceso de interpolacion

--- 04 - CADENAS DE CARACTERES/main.py
+++ 04 - CADENAS DE CARACTERES/main.py
@@ -31,7 +31,7 @@ print(caracter2)
 
 # 6. Longitud
 longitudString = len(sal)
-print(f'La longitud de la cadena "{sal}" es de {longitudString}')
+print(f'La longitud de la cadena "{sal}" es de {longitudString}') # Interpolacion utilizando f-string y {}
 
 # 7. Recorrido
 for i in range(longitudString):
@@ -45,24 +45,24 @@ print(salMinus)
 
 # 9. Reemplazo
 salReplace = sal
-print(f'Palabra Original -> {salReplace}')
+print(f'Palabra Original -> {salReplace}') # Interpolacion utilizando f-string y {}
 salReplace = salReplace.replace("o", "2") # Reemplaza todas las "o" por un "2"
-print(f'Primer reemplazo de letra "0" por un "2" -> {salReplace}')
+print(f'Primer reemplazo de letra "0" por un "2" -> {salReplace}') # Interpolacion utilizando f-string y {}
 salReplace = salReplace.replace("2", "o", 1) # Reemplaza el primer "2" por una "o"
-print(f'Segundo reemplazo, primer "2" por una "o" -> {salReplace}')
+print(f'Segundo reemplazo, primer "2" por una "o" -> {salReplace}') # Interpolacion utilizando f-string y {}
 
 # 10. Divison o Separacion
 salDiv = sal
-print(f'Palabra Original -> {salDiv}')
+print(f'Palabra Original -> {salDiv}') # Interpolacion utilizando f-string y {}
 salDiv = salDiv.split(",") # Separa la cadena por el caracter "," y los deja en una lista
-print(f'Separacion = {salDiv}')
+print(f'Separacion = {salDiv}') # Interpolacion utilizando f-string y {}
 salDiv = sal + " Me llamo Johan, soy ingeneriero electronico, me gusta el futbol, la programacion y la f1"
-print(f'Otra separacion de la siguiente cadena -> {salDiv}')
+print(f'Otra separacion de la siguiente cadena -> {salDiv}') # Interpolacion utilizando f-string y {}
 salDiv = salDiv.split(",", 2) # Separa por comas solo las dos primeras comas
-print(f'Separacion = {salDiv}')
+print(f'Separacion = {salDiv}') # Interpolacion utilizando f-string y {}
 
 # 11. Union
 myStringList = ["uno", "dos", "tres", "cuatro"]
-print(f'Lista -> {myStringList}')
+print(f'Lista -> {myStringList}') # Interpolacion utilizando f-string y {}
 myString = ", ".join(myStringList)
-print(f'Lista unida en un string -> {myString}')
+print(f'Lista unida en un string -> {myString}') # Interpolacion utilizando f-string y {}
