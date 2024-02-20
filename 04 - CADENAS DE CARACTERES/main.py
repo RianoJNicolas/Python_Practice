4f83602c8433c63443c5cc6f2ed12929811036a8
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-02-19 21:38:41 -0500

    Actualizando Reto #04, agregando verificacion palindromo al ejercicio extra

--- 04 - CADENAS DE CARACTERES/main.py
+++ 04 - CADENAS DE CARACTERES/main.py
@@ -74,3 +74,36 @@ if letra in sal:
     print(f"La letra {letra} si esta en {sal}")
 else:
     print(f"La letra {letra} no esta en {sal}")
+
+
+# DIFICULTAD EXTRA
+def palindromo(palabra):
+    palabra = palabra.replace(' ', '')
+    palabra = palabra.lower()
+    palabra_inver = palabra[::-1]
+
+    numeros = ["0","1","2","3","4","5","6","7","8","9"]
+
+    try:
+        for index in numeros:
+            if index in palabra:
+                raise ValueError("Por favor ingresa una palabra, por lo menos hay un numero dentro de ella") 
+        return palabra == palabra[::-1]
+    
+    except ValueError as ve:
+        print(ve)
+        return False
+
+
+def run():
+    palabra = input('Ingresa una palabra: ')
+    es_palindromo = palindromo(palabra)
+
+    if es_palindromo == True:
+        print(palabra + ', es palindroma')
+    else:
+        print(palabra + ', NO es palindroma')
+
+
+if __name__ == "__main__":
+    run()
