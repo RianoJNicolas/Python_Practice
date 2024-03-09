e40735cb45b961d154a04283939758f8d40508ef
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-03-08 20:40:59 -0500

    Actualizando Reto #05, Agregando posible ejemplo por valor

--- #05 - VALOR Y REFERENCIA/main.py
+++ #05 - VALOR Y REFERENCIA/main.py
@@ -1,5 +1,8 @@
 # Asignacion de variables por valor
-
+def fillCoupValue(fill):
+    fill = 99
+    initialLevel = 0
+    return initialLevel + fill
 
 # Asignacion de variables por referencia
 def fillCoup(fill):
