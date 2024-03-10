8c554bb057389cd62dacfd3074019688cf94712e
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-03-09 23:50:43 -0500

    Actualizando Reto #05, Agregando estructura base del ejercicio adicional

--- #05 - VALOR Y REFERENCIA/main.py
+++ #05 - VALOR Y REFERENCIA/main.py
@@ -10,4 +10,18 @@ def fillCoup(fill):
     initialLevel = 0
     return initialLevel + level
 
-# 
+# Dificultad Extra
+def caso1(a, b):
+    pass
+
+
+def caso2(a, b):
+    pass
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
