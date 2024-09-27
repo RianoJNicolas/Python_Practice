0c01eb494ed225480d0ebcd4242dad5f6873bfd0
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-09-27 17:17:04 -0500

    Update #06, Adding solution calculate factorial n

--- #06 - RECURSIVIDAD/main.py
+++ #06 - RECURSIVIDAD/main.py
@@ -9,6 +9,14 @@ def print_numbers(n):
         print(n)
         print_numbers(n-1)
 
-print_numbers(100)  # Imprime los números del 100 al 0
+# print_numbers(100)  # Imprime los números del 100 al 0
 
+# DIFICULTAD EXTRA:
+# 1. Factorial de un numero n
+def calc_factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n*calc_factorial(n-1)
 
+print(calc_factorial(5))  # Imprime el factorial de 5
