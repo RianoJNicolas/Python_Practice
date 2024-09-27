9ce44b445a1de791363f51745215f18d13d75f09
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-09-27 17:08:19 -0500

    Update #06, Adding solution excercise

--- #06 - RECURSIVIDAD/main.py
+++ #06 - RECURSIVIDAD/main.py
@@ -2,3 +2,13 @@
 # Dev: rianojnicolas              #
 ###################################
 
+# EJERCICIO:
+# función recursiva que imprima números del 100 al 0.
+def print_numbers(n):
+    if n >= 0:
+        print(n)
+        print_numbers(n-1)
+
+print_numbers(100)  # Imprime los números del 100 al 0
+
+
