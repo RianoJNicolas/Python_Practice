03e7232492f3e2f85e801a4b1ca9d966117f1fe8
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-10-02 18:13:05 -0500

    Adding LIFO method in challenge #07

--- #07 - PILAS Y COLAS/main.py
+++ #07 - PILAS Y COLAS/main.py
@@ -2,3 +2,31 @@
 # Dev: rianojnicolas              #
 ###################################
 
+# EJERCICIO:
+
+## Listas como Pilas - LIFO (last in first out)
+def stackMethod(stack, action, element=None):
+    """
+    Inputs:
+        stack: lista de elementos
+        action: accion a realizar "push"/"pop"/"peek"
+        element: elemento a insertar/eliminar/devolver ultimo elemento
+    Outputs:    
+        stack: lista de elementos
+    """
+    if action == "push":
+        stack.append(element)
+        return stack
+    elif action == "pop":
+        stack.pop()
+        return stack
+    elif action == "peek":
+        return stack[len(stack)-1]
+
+myStack = [1,2,3,4,5]
+print(stackMethod(myStack, "push", 6))
+print(stackMethod(myStack, "pop"))
+print(stackMethod(myStack, "peek"))
+
+## Listas como Colas - FIFO (first in first out)
+
